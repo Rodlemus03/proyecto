10a45a8c4c7b85da5b83a9e4de153daeee03acfc
Julio Lemus <rodlemus03@gmail.com>
2022-08-05 10:45:21 -0500

    Version con validacion de roles, usuarios.

--- gentelella-master/gentelella-master/production/php/login.php
+++ gentelella-master/gentelella-master/production/php/login.php
@@ -1,6 +1,8 @@
 <?php
 include("conexion.php");
 global $conexion;
+$rol="regular";
+$rolCotejo="";
 if(isset($_POST['b1'])){
     $usuario=$_POST['usuarioIngresar'];
     $password=$_POST['passwordIngresar'];
@@ -11,9 +13,16 @@ if(isset($_POST['b1'])){
     $result=mysqli_query($conexion,$consultaSQL);
     while($r=mysqli_fetch_array($result)){
         if($usuario==$r['usuario'] and $password==$r['password']){
-            $bandera=1;}
+            $bandera=1;
+        $rolCotejo=$r['rol'];}
     }
-    if($bandera==1){echo "<script>window.location='../administrativo.html';</script>";}else{echo "<script>alert('Credenciales incorrectas ');window.location='../login.html'</script>";}
+    if($bandera==1){
+        if($rolCotejo=='administrador'){
+            echo "<script>window.location='../administrativo.html';</script>";
+        }else if($rolCotejo="regular"){
+            echo "<script>window.location='../regular.html';</script>";
+        }
+    }else{echo "<script>alert('Credenciales incorrectas ');window.location='../login.html'</script>";}
 
 }if(isset($_POST['b2'])){
     $dominio="@ajsolutions.com";
@@ -21,7 +30,7 @@ if(isset($_POST['b1'])){
     $newPass=$_POST['passNueva'];
     $nuevoUsuario=$newName.$dominio;
     echo $nuevoUsuario;
-    $consultaSQL2="INSERT INTO usuariosmuni(usuario,password) VALUES('$nuevoUsuario','$newPass')";
+    $consultaSQL2="INSERT INTO usuariosmuni(usuario,password,rol) VALUES('$nuevoUsuario','$newPass','$rol')";
     $result=mysqli_query($conexion,$consultaSQL2);
     if($result){echo "<script>alert('Usuario creado correctamente');window.location='../login.html'</script>";}
 }
